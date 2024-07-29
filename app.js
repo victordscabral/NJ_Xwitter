c416215b9d23c9a8635f336c4bc9569dcbc378e4
Victor Cabral <87657291+victordscabral@users.noreply.github.com>
2024-07-29 03:48:20 -0300

    Atualizando página de login e registro

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3003;
 const middleware = require('./middleware')
+const path = require('path')
+const bodyParser = require("body-parser")
 
 const server = app.listen(port, () => console.log("Servidor ouvindo na porta " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
 const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
 
 app.use("/login", loginRoute);
-
+app.use("/register", registerRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
