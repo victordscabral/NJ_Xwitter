6bb344904a29f3282e286676bb191716abf6721f
Victor Cabral <87657291+victordscabral@users.noreply.github.com>
2024-07-30 01:27:59 -0300

    Adicionando histórico de postagens

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,7 +8,14 @@ const Post = require('../../schemas/PostSchema');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
-    
+    Post.find()
+    .populate("postedBy")
+    .sort({ "createdAt": -1 })
+    .then(results => res.status(200).send(results))
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
 })
 
 router.post("/", async (req, res, next) => {
